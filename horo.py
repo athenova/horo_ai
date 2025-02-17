19780aaa110addd32af01f72515c056df5a762ba
athenova <aleksey.sergeyev@yandex.com>
2025-02-17 17:43:34 +0300

    Добавлен заголовок

--- horo.py
+++ horo.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.environ.get(BOT_TOKEN_NAME)
 #CHAT_ID = -1002374309134
 CHAT_ID = '@horo_ai'
 
-def job(sign):
+def job(sign, symbol):
     client = OpenAI()
     text = client.chat.completions.create(
         model=AI_TEXT_MODEL,
@@ -20,20 +20,24 @@ def job(sign):
         ]
     ).choices[0].message.content
     bot = telebot.TeleBot(BOT_TOKEN)
+    text = f"""{symbol} **{sign}** {symbol}
+
+{text}    
+"""
     bot.send_message(chat_id=CHAT_ID, text=text, parse_mode="Markdown")
 
-schedule.every().day.at("08:00",'Europe/Moscow').do(job, sign="Рыбы")
-#schedule.every().day.at("08:01",'Europe/Moscow').do(job, sign="Овен")
-#schedule.every().day.at("08:02",'Europe/Moscow').do(job, sign="Телец")
-#schedule.every().day.at("08:03",'Europe/Moscow').do(job, sign="Близнецы")
-#schedule.every().day.at("08:04",'Europe/Moscow').do(job, sign="Рак")
-#schedule.every().day.at("08:05",'Europe/Moscow').do(job, sign="Лев")
-#schedule.every().day.at("08:06",'Europe/Moscow').do(job, sign="Дева")
-#schedule.every().day.at("08:07",'Europe/Moscow').do(job, sign="Весы")
-#schedule.every().day.at("08:08",'Europe/Moscow').do(job, sign="Скорпион")
-#schedule.every().day.at("08:09",'Europe/Moscow').do(job, sign="Стрелец")
-#schedule.every().day.at("08:10",'Europe/Moscow').do(job, sign="Козерог")
-#schedule.every().day.at("08:11",'Europe/Moscow').do(job, sign="Водолей")
+schedule.every().day.at("08:00",'Europe/Moscow').do(job, sign="Рыбы", symbol='♓')
+schedule.every().day.at("08:01",'Europe/Moscow').do(job, sign="Овен", symbol='♈')
+schedule.every().day.at("08:02",'Europe/Moscow').do(job, sign="Телец", symbol='♉')
+schedule.every().day.at("08:03",'Europe/Moscow').do(job, sign="Близнецы", symbol='♊')
+schedule.every().day.at("08:04",'Europe/Moscow').do(job, sign="Рак", symbol='♋')
+schedule.every().day.at("08:05",'Europe/Moscow').do(job, sign="Лев", symbol='♌')
+schedule.every().day.at("08:06",'Europe/Moscow').do(job, sign="Дева", symbol='♍')
+schedule.every().day.at("08:07",'Europe/Moscow').do(job, sign="Весы", symbol='♎')
+schedule.every().day.at("08:08",'Europe/Moscow').do(job, sign="Скорпион", symbol='♏')
+schedule.every().day.at("08:09",'Europe/Moscow').do(job, sign="Стрелец", symbol='♐')
+schedule.every().day.at("08:10",'Europe/Moscow').do(job, sign="Козерог", symbol='♑')
+schedule.every().day.at("08:11",'Europe/Moscow').do(job, sign="Водолей", symbol='♒')
 
 fifteen_minutes = 15 * 60
 
